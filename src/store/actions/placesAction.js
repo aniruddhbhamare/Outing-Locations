de9e4c66a028f502b228b5f45f48b7092ed3e05e
Aniruddha Bhamare <ani.fullstack@gmail.com>
2018-12-11 05:03:17 -0800

    1> store placeName and location to server / 2> added the cloud function for extracting image and store to server

--- src/store/actions/placesAction.js
+++ src/store/actions/placesAction.js
@@ -2,38 +2,33 @@ import {ADD_PLACE,DELETE_PLACE,} from './actionTypes';
 //SELECT_PLACE,DESELECT_PLACE
 
 //for database
-// export const addPlace = (placeName,location,image) =>{
-//     return dispatch =>{
-//         const placeData ={
-//             name:placeName,
-//             location:location
-//         };
-//         fetch("https://places-72147.firebaseio.com/places.json",{
-//             method: "POST",
-//             body:JSON.stringify(placeData)
-//         })
-//         .catch(err => console.log(err))
-//         .then(res => res.json())
-//         .then(parsedRes =>{
-//             console.log(parsedRes);
-//         }); 
-//     };
-// };
-
-// export const deletePlace = (key) =>{
-//     return{
-//         type:DELETE_PLACE,
-//         selectPlace:key
-//     };
-// };
-
-//for local storage
 export const addPlace = (placeName,location,image) =>{
-    return {
-        type:ADD_PLACE,
-        placeName:placeName,
-        location:location,
-        image:image
+    return dispatch =>{
+        
+        fetch("https://us-central1-places-72147.cloudfunctions.net/storeImage",{
+            method:"POST",
+            body:JSON.stringify({
+                image:image.base64
+            })
+        })
+         .catch(err => console.log(err))
+         .then(res => res.json())
+         .then(parsedRes =>{
+            const placeData ={
+                name:placeName,
+                location:location,
+                image:parsedRes.imageUrl
+            };
+             return fetch("https://places-72147.firebaseio.com/places.json",{
+                method: "POST",
+                body:JSON.stringify(placeData)
+            })
+         })
+         .catch(err => console.log(err))
+        .then(res => res.json())
+        .then(parsedRes =>{
+            console.log(parsedRes);
+        }); 
     };
 };
 
@@ -43,6 +38,23 @@ export const deletePlace = (key) =>{
         selectPlace:key
     };
 };
+
+//for local storage
+// export const addPlace = (placeName,location,image) =>{
+//     return {
+//         type:ADD_PLACE,
+//         placeName:placeName,
+//         location:location,
+//         image:image
+//     };
+// };
+
+// export const deletePlace = (key) =>{
+//     return{
+//         type:DELETE_PLACE,
+//         selectPlace:key
+//     };
+// };
 
 // export const selectPlace = (key) =>{
 //     return {
