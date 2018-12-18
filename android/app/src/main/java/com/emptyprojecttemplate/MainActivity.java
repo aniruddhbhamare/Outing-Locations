00e16d7aaaca77e292f6571d183b38b96c2cf11c
Aniruddha Bhamare <ani.fullstack@gmail.com>
2018-12-17 23:27:17 -0800

    added splash screen

--- android/app/src/main/java/com/emptyprojecttemplate/MainActivity.java
+++ android/app/src/main/java/com/emptyprojecttemplate/MainActivity.java
@@ -1,9 +1,13 @@
 package com.emptyprojecttemplate;
 
 import com.reactnativenavigation.controllers.SplashActivity;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.graphics.Color;
+import android.view.Gravity;
+import android.util.TypedValue;
 
 public class MainActivity extends SplashActivity {
-
     // /**
     //  * Returns the name of the main component registered from JavaScript.
     //  * This is used to schedule rendering of the component.
@@ -12,4 +16,24 @@ public class MainActivity extends SplashActivity {
     // protected String getMainComponentName() {
     //     return "emptyprojecttemplate";
     // }
+    @Override
+    public LinearLayout createSplashLayout() {
+        LinearLayout view = new LinearLayout(this);
+        TextView textView = new TextView(this);
+        
+        view.setBackgroundColor(Color.parseColor("#BA68C8"));
+        view.setGravity(Gravity.CENTER);
+
+        textView.setTextColor(Color.parseColor("#ffffff"));
+        textView.setText("Beautiful   Places" );
+        textView.setGravity(Gravity.CENTER);
+        textView.setTextSize(TypedValue.COMPLEX_UNIT_DIP,55);
+
+        // textView.setTextColor(Color.parseColor("#ffffff"));
+        // textView.setText("Places");
+        // textView.setTextSize(TypedValue.COMPLEX_UNIT_DIP,50);
+        
+        view.addView(textView);
+        return view;
+    }
 }
